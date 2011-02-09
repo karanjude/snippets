3aabd176edc56d346ff973785dfdea952cc5739d
karan.jude@gmail.com <karan.jude@gmail.com@737fc9ef-4d2d-0410-97ed-4d0c502f76d2>
2011-02-09 11:40:20 +0000

    refactored code

--- ai/a.cc
+++ ai/a.cc
@@ -52,11 +52,12 @@ void print_maze(vector< pair<int,int> > with_path = vector< pair<int,int> >()){
   }
 }
 
-struct node{
+class SearchNode{
+public:
   int x,y;
   vector< pair<int, int> > path_so_far;
 
-  node(int x_, int y_, const vector< pair<int,int> > &  path_so_far_):
+  SearchNode(int x_, int y_, const vector< pair<int,int> > &  path_so_far_ ):
     x(x_),
     y(y_),
     path_so_far(path_so_far_)
@@ -64,59 +65,125 @@ struct node{
     path_so_far.push_back(make_pair(x,y));
   }
 
-  node(const node& other){
+  SearchNode(const SearchNode& other){
     x = other.x;
     y = other.y;
     path_so_far = other.path_so_far;
   }
-};
-	    
-	   
 
-void dfs(){
-  stack< node > s;
-  s.push(node(sx,sy, vector< pair<int,int> >()));
-  
-  int dx[] = {-1,1,0,0};
-  int dy[] = {0,0,-1,1};
-  
-  while(!s.empty()){
-    node p = s.top();
-    s.pop();
-
-    cout << endl << "processing .. " << p.x << " " << p.y;
-
-    if(visited.find(make_pair(p.x, p.y)) != visited.end())
-       continue;
-
-    visited.insert(make_pair(make_pair(p.x, p.y) ,true));
-
-    if(ex == p.x && ey == p.y){
-      cout << endl << "FOUND DESTINATION";
-      for(vector< pair<int,int> >::iterator i = p.path_so_far.begin(); i != p.path_so_far.end(); i++){
+  void print_path() const {
+   for(vector< pair<int,int> >::const_iterator i = path_so_far.begin(); i != path_so_far.end(); i++){
 	cout << endl << "(" << i->first << " , " << height - 1 - i->second << ")";
-      }
-      print_maze(p.path_so_far);
-      break;
+    }
+    print_maze(path_so_far);
+  }
+
+};
+
+
+  class SearchStrategy{
+  public:
+    virtual bool should_search_more() = 0;
+    virtual SearchNode get_next_search_node() = 0;
+    virtual bool has_reached_goal_node(const SearchNode& ) = 0;
+    virtual bool can_generate_search_node_at(int x, int y) = 0;
+    virtual void generate_search_nodes(const SearchNode& ) = 0;
+    virtual bool should_process_node(const SearchNode& ) = 0;
+    virtual void mark_node(const SearchNode& ) = 0;
+    virtual void unmark_node(const SearchNode& ) = 0;
+    virtual void process_end_node(const SearchNode& ) = 0;
+  };
+
+  class DFS : public SearchStrategy {
+  private:
+    stack<SearchNode> s;
+    map< pair<int, int>, bool > visited;
+    int start_x, start_y, end_x, end_y;
+    
+  public:
+    DFS(int start_x_, int start_y_, int end_x_, int end_y_):
+      start_x(start_x_),
+      start_y(start_y_),
+      end_x(end_x_),
+      end_y(end_y_)
+    {
+      s.push(SearchNode(start_x, start_y, vector< pair<int,int> >()));
     }
 
-    for(int i = 0; i < 4;i++){
-      int xx = p.x + dx[i];
-      int yy = p.y - dy[i];
-      if (xx < 0 || xx > width-1 || yy < 0 || yy > height-1 )
-	continue;
-      
-      cout << endl << "v : at " << xx << " " << yy << " " << maze[yy][xx];
-      if (maze[yy][xx] < 0)
-	continue;
-      
-      s.push(node(xx,yy, p.path_so_far));
+    bool should_search_more(){
+      return !s.empty();
+    }
+
+    SearchNode get_next_search_node(){
+      SearchNode node = s.top();
+      s.pop();
+      return node;
+    }
+
+    bool should_process_node(const SearchNode& p){
+      return visited.find(make_pair(p.x, p.y)) == visited.end();
+    }
+
+    void mark_node(const SearchNode& p){
+      visited.insert(make_pair(make_pair(p.x, p.y) ,true));
+    }
+
+    bool has_reached_goal_node(const SearchNode& p ){
+      return (end_x == p.x && end_y == p.y);
+    }
+
+    void process_end_node(const SearchNode& p){
+      cout << endl << "FOUND DESTINATION";
+      p.print_path();
+    }
+
+    void unmark_node(const SearchNode& p){
       visited[make_pair(p.x, p.y)] = false;
     }
-    
+
+    bool can_generate_search_node_at(int xx , int yy){
+      return xx < 0 || xx > width-1 || yy < 0 || yy > height-1;
+    }
+
+    void generate_search_nodes(const SearchNode& p){
+      int dx[] = {-1,1,0,0};
+      int dy[] = {0,0,-1,1};
+
+      for(int i = 0; i < 4;i++){
+	int xx = p.x + dx[i];
+	int yy = p.y - dy[i];
+	if(can_generate_search_node_at(xx, yy))
+	  continue;
+      
+	cout << endl << "v : at " << xx << " " << yy << " " << maze[yy][xx];
+	if (maze[yy][xx] < 0)
+	  continue;
+      
+	s.push(SearchNode(xx,yy, p.path_so_far));
+	//visited[make_pair(p.x, p.y)] = false;
+      }
+    }
+
+  };
+	  
+void search(SearchStrategy& search_strategy){
+  while(search_strategy.should_search_more()){
+    SearchNode p = search_strategy.get_next_search_node();
+
+    cout << endl << "processing .. " << p.x << " " << p.y;
+   
+    if(!search_strategy.should_process_node(p))
+      continue;
+
+    search_strategy.mark_node(p);
+    if(search_strategy.has_reached_goal_node(p)){
+      search_strategy.process_end_node(p);
+      break;
+    }
+    search_strategy.generate_search_nodes(p);
+    search_strategy.unmark_node(p);
   }
-		  
-}
+}	   
 
 int main(int argc, char** argv){
   char * file_name = NULL;
@@ -168,7 +235,10 @@ int main(int argc, char** argv){
   }
 
   print_maze();
-  dfs();
+  //dfs();
+
+  DFS dfs(sx, sy, ex, ey);
+  search(dfs);
 
   file.close();
   return 0;
